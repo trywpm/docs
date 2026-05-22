fb649732607f2be2ccd9397d2f0bd496c0962925
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-05-23 04:21:22 +0530

    Add components

---
+++ components/search.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import type { SharedProps } from 'fumadocs-ui/components/dialog/search';
+
+import { create } from '@orama/orama';
+import {
+  SearchDialog,
+  SearchDialogIcon,
+  SearchDialogList,
+  SearchDialogInput,
+  SearchDialogClose,
+  SearchDialogHeader,
+  SearchDialogContent,
+  SearchDialogOverlay,
+} from 'fumadocs-ui/components/dialog/search';
+import { useI18n } from 'fumadocs-ui/contexts/i18n';
+import { useDocsSearch } from 'fumadocs-core/search/client';
+
+function initOrama() {
+  return create({
+    schema: { _: 'string' },
+    language: 'english',
+  });
+}
+
+export default function DefaultSearchDialog(props: SharedProps) {
+  const { locale } = useI18n();
+  const { search, setSearch, query } = useDocsSearch({
+    type: 'static',
+    initOrama,
+    locale,
+  });
+
+  return (
+    <SearchDialog search={search} onSearchChange={setSearch} isLoading={query.isLoading} {...props}>
+      <SearchDialogOverlay />
+      <SearchDialogContent>
+        <SearchDialogHeader>
+          <SearchDialogIcon />
+          <SearchDialogInput />
+          <SearchDialogClose />
+        </SearchDialogHeader>
+        <SearchDialogList items={query.data !== 'empty' ? query.data : null} />
+      </SearchDialogContent>
+    </SearchDialog>
+  );
+}
