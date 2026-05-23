8554d5e9ba707bbfce2d9d61cb5eb3c912051591
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-05-23 18:56:39 +0530

    Add site url

--- lib/shared.ts
+++ lib/shared.ts
@@ -3,6 +3,9 @@ export const docsRoute = '/';
 export const docsImageRoute = '/og/docs';
 export const docsContentRoute = '/llms.mdx/docs';
 
+export const siteUrl = 'https://wpm.so';
+export const docsPathPrefix = '/docs';
+
 export const gitConfig = {
   user: 'trywpm',
   repo: 'docs',
