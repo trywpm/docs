f6a45177d52b2ab78f01539dcdd052db0335ede1
thelovekesh <lovekesh.kumar@rtcamp.com>
2026-05-24 14:37:52 +0530

    Add algolia app info

--- lib/shared.ts
+++ lib/shared.ts
@@ -6,7 +6,9 @@ export const docsContentRoute = '/llms.mdx/docs';
 export const siteUrl = 'https://wpm.so';
 export const docsPathPrefix = '/docs';
 
+export const algoliaAppId = '4AHXXH3M9O';
 export const algoliaIndexName = 'wpm-site';
+export const algoliaSearchKey = '00d66ae9eaa87cd80e4aeecbda7fe8fd';
 
 export const gitConfig = {
   user: 'trywpm',
